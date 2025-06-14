cdb97a2094a3139b313f15ba15e186d8531cc530
Adlai さ ん <126016337+AdlaiSantopadre@users.noreply.github.com>
2025-06-14 15:53:58 +0200

    Update transitions.py

--- fsm/transitions.py
+++ fsm/transitions.py
@@ -62,3 +62,37 @@ for s in states.values():
         data.append((s.name, evt, next_s.name))
 df_transitions = pd.DataFrame(data, columns=["Stato Attuale", "Evento", "Stato Successivo"])
 import ace_tools as tools; tools.display_dataframe_to_user(name="Tabella delle transizioni FSM", dataframe=df_transitions)
+# Definizione transizioni + azioni MQTT (mock topic/action)
+states["S_4_4"].add_transition("nodo_1", states["S_1_4"], actions=["log:passaggio nodo_1"])
+states["S_1_4"].add_transition("nodo_2", states["S_2_4"], actions=["log:passaggio nodo_2"])
+states["S_1_4"].add_transition("nodo_1", states["S_1_1"], actions=["log:passaggio nodo_1"])
+states["S_2_4"].add_transition("nodo_1", states["S_2_1"], actions=["log:passaggio nodo_1"])
+states["S_2_4"].add_transition("nodo_3", states["S_3_4"], actions=["log:passaggio nodo_3"])
+states["S_3_4"].add_transition("nodo_1", states["S_3_1"], actions=["log:warning"])
+states["S_3_4"].add_transition("nodo_4", states["S_4_4"], actions=["halt"])
+states["S_1_1"].add_transition("nodo_2", states["S_2_1"], actions=["log:passaggio nodo_2"])
+states["S_2_1"].add_transition("nodo_2", states["S_2_2"], actions=["log:passaggio nodo_2"])
+states["S_2_1"].add_transition("nodo_3", states["S_3_1"], actions=["log:passaggio nodo_3"])
+states["S_3_1"].add_transition("nodo_3", states["S_3_2"], actions=["log:passaggio nodo_3"])
+states["S_3_1"].add_transition("nodo_4", states["S_4_1"], actions=["log:passaggio nodo_4", "set:warning=True"])
+states["S_4_1"].add_transition("nodo_1", states["D_1_1"], actions=["mqtt:binario/deviatoio01=1", "wait_ack", "set:deviato=True"])
+states["D_1_1"].add_transition("nodo_2", states["D_1_2"], actions=["log:L su nodo 2"])
+states["D_1_1"].add_transition("nodo_5", states["D_2_1"], actions=["log:V deviato su nodo 5", "mqtt:binario/deviatoio01=0", "set:warning=False"])
+states["D_1_2"].add_transition("nodo_3", states["D_1_3"], actions=["log:L su nodo 3"])
+states["D_1_2"].add_transition("nodo_5", states["D_2_2"], actions=["log:V deviato su nodo 5", "mqtt:binario/deviatoio01=0", "set:warning=False"])
+states["D_1_3"].add_transition("nodo_4", states["D_1_4"], actions=["log:L su nodo 4"])
+states["D_1_3"].add_transition("nodo_5", states["D_2_3"], actions=["log:V deviato su nodo 5"])
+states["D_1_4"].add_transition("nodo_1", states["D_1_1_loop"], actions=["log:L su nodo 1"])
+states["D_1_4"].add_transition("nodo_5", states["D_2_4"], actions=["log:V deviato su nodo 5"])
+states["D_2_1"].add_transition("nodo_2", states["D_2_2"], actions=["log:L su nodo 2"])
+states["D_2_2"].add_transition("nodo_3", states["D_2_3"], actions=["log:L su nodo 3"])
+states["D_2_3"].add_transition("nodo_4", states["D_2_4"], actions=["log:L su nodo 4"])
+
+# Generazione tabella con azioni
+data = []
+for state in states.values():
+    for event, next_state in state.transitions.items():
+        azioni = "; ".join(state.actions.get(event, []))
+        data.append((state.name, event, next_state.name, azioni))
+
+df = pd.DataFrame(data, columns=["Stato Attuale", "Evento", "Stato Successivo", "Azioni MQTT / Log"])
