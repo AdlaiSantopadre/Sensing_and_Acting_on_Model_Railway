1da561aa8d704266f644a93ca3e5b92eb542eb12
Adlai さ ん <126016337+AdlaiSantopadre@users.noreply.github.com>
2025-06-20 10:33:47 +0200

    renamed:    client_acting_and_logic/main.py -> client_acting_and_logic/old_main.py 	modified:   main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import threading
-from mqtt_ack_handler import start_ack_handler
-from mqtt_servomotori import start_acting_client
+from client_acting_and_logic.mqtt_ack_handler import start_ack_handler
+from client_acting_and_logic.mqtt_servomotore import start_acting_client
 from fsm.fsm_engine_mqtt import FSMEngine
 from fsm.states import states
 from fsm.transitions import setup_transitions
-from event_stack import pop_event
+# Ensure event_stack.py exists in the same directory or update the import path accordingly
+from client_acting_and_logic.event_stack import pop_event
 import time
 
 # Inizializza FSM
