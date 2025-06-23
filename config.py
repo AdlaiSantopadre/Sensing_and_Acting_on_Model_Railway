bb03fcc4fd917212a9d1cb0d43acc8bcbc8f33af
Adlai さ ん <126016337+AdlaiSantopadre@users.noreply.github.com>
2025-06-23 19:07:31 +0200

    modified:   config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ PASSWORD ="esp2025"
 # Lista dei nodi ESP8266 attivi
 CLIENT_IDS = [f"Nodo0{i}" for i in range(1, 6)]  # Nodo01 ... Nodo05
 
-TOPIC_ACK_IN = "/binario/01"
-TOPIC_ACK_OUT = "ack/Nodo01"
+#TOPIC_ACK_IN = "/binario/01"
+#TOPIC_ACK_OUT = "ack/Nodo01"
 # Mapping dei topic MQTT per ciascun nodo
 TOPIC_ACK_IN = {cid: f"/binario/0{cid[-1]}" for cid in CLIENT_IDS}
 TOPIC_ACK_OUT = {cid: f"ack/Nodo0{cid}" for cid in CLIENT_IDS}
